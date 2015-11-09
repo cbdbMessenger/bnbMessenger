d35097663afdfa255e22c051edcd2af63e2f56ee
catbreaddogbutter <rfergerson@gmail.com>
2015-11-08 22:50:00 -0500

    added code to start service - not complete yet

--- app/src/main/java/cbdb/chatapp/ConnectActivity.java
+++ app/src/main/java/cbdb/chatapp/ConnectActivity.java
@@ -1,16 +1,20 @@
 package cbdb.chatapp;
 
 import android.app.Activity;
+import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
 import android.net.wifi.WifiManager;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.nsd.WifiP2pServiceInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,12 +30,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 
 public class ConnectActivity extends Activity {
 
+    public static final String SERVICE_TYPE = "_http._tcp.";
     Button toggle, search, connect;
 
     private WifiManager wifiManager;
@@ -40,6 +48,15 @@ public class ConnectActivity extends Activity {
     private Channel channel;
 
     private List peers;
+
+    private WifiP2pServiceInfo chatServiceInfo;
+    private NsdServiceInfo mService;
+    private ServerSocket socket;
+    private String serviceName;
+    private NsdManager.RegistrationListener registrationListener;
+    private NsdManager.DiscoveryListener discoveryListener;
+    private NsdManager.ResolveListener resolveListener;
+    private NsdManager nsdManager;
 
     public static final File directory = new File("/sdcard/ChatAppTemp");
     public static File outFile = new File(directory, "DeviceList");
@@ -52,7 +69,9 @@ public class ConnectActivity extends Activity {
         buttonSetup();
         intentFilterSetup();
         wifiSetup();
-
+        initializeRegistrationListener();
+        initializeDiscoveryListener();
+        initializeResolveListener();
     }
 
 //    @Override
@@ -78,12 +97,12 @@ public class ConnectActivity extends Activity {
 //    }
 
     private void buttonSetup() {
-        toggle = (Button)findViewById(R.id.btnWifiToggle);
+        toggle = (Button) findViewById(R.id.btnWifiToggle);
         toggle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(!wifiManager.isWifiEnabled())
+                if (!wifiManager.isWifiEnabled())
                     wifiManager.setWifiEnabled(true);
                 else
                     wifiManager.setWifiEnabled(false);
@@ -91,10 +110,11 @@ public class ConnectActivity extends Activity {
             }
         });
 
-        search = (Button)findViewById(R.id.btnSearch);
+        search = (Button) findViewById(R.id.btnSearch);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 
                 wifiP2pManager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
 
@@ -117,7 +137,34 @@ public class ConnectActivity extends Activity {
             }
         });
 
+        toggle = (Button) findViewById(R.id.btnConnect);
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isHost = true;
+                if (isHost) {
+                    try {
+                        socket = new ServerSocket(0); // TODO: randomize?  find new address if in use
+                        int localPort = socket.getLocalPort();
+                        System.out.println("set up server socket: " + localPort);
+                        registerService(localPort);
+
+                    } catch (Exception e) {
+                        System.out.println("Error setting up socket " + e);
+                    }
+                } else {
+                    // Client
+                    nsdManager.discoverServices(
+                            SERVICE_TYPE, NsdManager.PROTOCOL_DNS_SD, discoveryListener);
+                }
+
+            }
+
+            ;
+
+        });
     }
+
 
     private void intentFilterSetup() {
         intentFilter = new IntentFilter();
@@ -147,18 +194,22 @@ public class ConnectActivity extends Activity {
                 int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
                 if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
                     showToast("wifi p2p enabled");
+                    //TODO: start service or join existing here?
                 } else {
                     showToast("wifi p2p disabled");
                 }
             } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
                 // Call WifiP2pManager.requestPeers() to get a list of current peers
-                if(wifiP2pManager != null) {
+                if (wifiP2pManager != null) {
                     wifiP2pManager.requestPeers(channel, peerListListener);
                 }
             } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
                 // Respond to new connection or disconnections
+                System.out.println("new connection changed action: " + action);
             } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
                 // Respond to this device's wifi state changing
+                System.out.println("this device changed action: " + action);
+
             }
 
         }
@@ -183,8 +234,8 @@ public class ConnectActivity extends Activity {
 
             try {
                 fos = new FileOutputStream(outFile);
-                for(int i=0 ; i<peers.size() ; i++) {
-                    fos.write((Integer.toString(i+1) + ": " + peers.get(i).toString() + "\n\n\n").getBytes());
+                for (int i = 0; i < peers.size(); i++) {
+                    fos.write((Integer.toString(i + 1) + ": " + peers.get(i).toString() + "\n\n\n").getBytes());
                 }
                 fos.close();
             } catch (FileNotFoundException e) {
@@ -196,5 +247,134 @@ public class ConnectActivity extends Activity {
 
         }
     };
+
+
+    private void registerService(int port) {
+        // Create the NsdServiceInfo object, and populate it.
+        NsdServiceInfo serviceInfo = new NsdServiceInfo();
+
+        // The name is subject to change based on conflicts
+        // with other services advertised on the same network.
+        serviceInfo.setServiceName("bnbChat");
+        serviceInfo.setServiceType(SERVICE_TYPE);
+        serviceInfo.setPort(port);
+
+        //TODO;  FIX THIS LINE 
+      //  nsdManager = (NsdManager)bnbMessenger.getSystemService(Context.NSD_SERVICE);
+
+        nsdManager.registerService(
+                serviceInfo, NsdManager.PROTOCOL_DNS_SD, registrationListener);
+    }
+
+    public void initializeRegistrationListener() {
+        registrationListener = new NsdManager.RegistrationListener() {
+
+            @Override
+            public void onServiceRegistered(NsdServiceInfo NsdServiceInfo) {
+                // Save the service name.  Android may have changed it in order to
+                // resolve a conflict, so update the name you initially requested
+                // with the name Android actually used.
+                serviceName = NsdServiceInfo.getServiceName();
+                System.out.println("service registered: "+serviceName);
+            }
+
+            @Override
+            public void onRegistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Registration failed!  Put debugging code here to determine why.
+                System.out.println("failure. couldn't register service");
+            }
+
+            @Override
+            public void onServiceUnregistered(NsdServiceInfo arg0) {
+                // Service has been unregistered.  This only happens when you call
+                // NsdManager.unregisterService() and pass in this listener.
+                System.out.println("service was unregistered");
+            }
+
+            @Override
+            public void onUnregistrationFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Unregistration failed.  Put debugging code here to determine why.
+                System.out.println("failure. unregistration failed");
+            }
+        };
+    }
+
+    public void initializeDiscoveryListener() {
+
+        // Instantiate a new DiscoveryListener
+        discoveryListener = new NsdManager.DiscoveryListener() {
+
+            //  Called as soon as service discovery begins.
+            @Override
+            public void onDiscoveryStarted(String regType) {
+                System.out.println("service discovery started");
+            }
+
+            @Override
+            public void onServiceFound(NsdServiceInfo service) {
+                // A service was found!  Do something with it.
+                System.out.println("discovered service " + service);
+                if (!service.getServiceType().equals(SERVICE_TYPE)) {
+                    // Service type is the string containing the protocol and
+                    // transport layer for this service.
+                    System.out.println( "unknown service sype: " + service.getServiceType());
+                } else if (service.getServiceName().equals(serviceName)) {
+                    // The name of the service tells the user what they'd be
+                    // connecting to. It could be "Bob's Chat App".
+                    System.out.println("same machine: " + serviceName);
+                } else if (service.getServiceName().contains("NsdChat")){
+                    nsdManager.resolveService(service, resolveListener);
+                }
+            }
+
+            @Override
+            public void onServiceLost(NsdServiceInfo service) {
+                // When the network service is no longer available.
+                // Internal bookkeeping code goes here.
+                System.out.println( "service lost" + service);
+            }
+
+            @Override
+            public void onDiscoveryStopped(String serviceType) {
+                System.out.println( "Discovery stopped: " + serviceType);
+            }
+
+            @Override
+            public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+               System.out.println( "Discovery failed: Error code:" + errorCode);
+                nsdManager.stopServiceDiscovery(this);
+            }
+
+            @Override
+            public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+                System.out.println( "Discovery failed: Error code:" + errorCode);
+                nsdManager.stopServiceDiscovery(this);
+            }
+        };
+    }
+
+    public void initializeResolveListener() {
+        resolveListener = new NsdManager.ResolveListener() {
+
+            @Override
+            public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
+                // Called when the resolve fails.  Use the error code to debug.
+                System.out.println("Resolve failed" + errorCode);
+            }
+
+            @Override
+            public void onServiceResolved(NsdServiceInfo serviceInfo) {
+                System.out.println( "Resolve Succeeded. " + serviceInfo);
+
+                if (serviceInfo.getServiceName().equals(serviceName)) {
+                    System.out.println("Same IP.");
+                    return;
+                }
+                mService = serviceInfo;
+                int port = mService.getPort();
+                InetAddress host = mService.getHost();
+            }
+        };
+    }
 
 }
